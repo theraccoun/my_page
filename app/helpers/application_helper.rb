39b6cb327295d3e591d89afefb938f8c0f656dc4
Steven MacCoun <theraccoun@gmail.com>
2013-03-20 01:09:23 -0600

    Initial personal styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+	def full_title(page_title)
+		base_title = "smaccoun"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+
+	def addColon(page_title)
+	  	if page_title.empty?
+	  		""
+	  	else
+	  		": "
+	  	end
+  end
 end
