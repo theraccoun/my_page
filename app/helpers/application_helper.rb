15b7d6ef704f27df1d2c2f473bcfb53f64a66c4c
Steven MacCoun <theraccoun@gmail.com>
2013-04-01 01:36:35 -0600

    Nav bar updates current page item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,12 @@ module ApplicationHelper
 	  		": "
 	  	end
   end
+
+  def nav_link(link_text, link_path)
+	  class_name = current_page?(link_path) ? 'current_page_item' : ''
+
+	  content_tag(:li, :class => class_name) do
+	    link_to link_text, link_path
+	  end
+	end
 end
